81d842d23aabc98399383463e9e9631a8448e3bb
zannat02 <shawon4166@gmail.com>
2026-08-25 12:46:17 +0600

    fix url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.DEV
     ? 'http://localhost:3000'
-    : 'https://notice-board-server.onrender.com/';
+    : 'https://notice-board-server.onrender.com';
 
 export default API_URL;
